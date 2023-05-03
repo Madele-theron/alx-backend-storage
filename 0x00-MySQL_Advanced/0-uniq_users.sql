e98a9fec4d169e55143292f88c8584dd7ff733c0
Madele <madele.theron@gmail.com>
2023-05-03 21:46:08 +0200

    task 0

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- A SQL Script that creates a user table:
+-- Create a table called "users"
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  `email` VARCHAR(255) UNIQUE NOT NULL,
+  `name` VARCHAR(255)
+);

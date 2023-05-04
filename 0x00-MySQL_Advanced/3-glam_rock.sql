0d7882c74aad3ffbdd1a48457670e60d25102eab
Madele <madele.theron@gmail.com>
2023-05-04 19:53:20 +0200

    add distinct

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,17 +1,6 @@
-
 -- A SQL Script that ranks country origins of bands by fans:
 -- Create a table called "users"
 
-SELECT band_name, IFNULL(split, NOW()) - formed as lifespan 
+SELECT DISTINCT band_name, DATEDIFF(IFNULL(split, NOW()), formed) as lifespan 
 FROM metal_bands WHERE FIND_IN_SET('Glam rock', style)
 ORDER BY lifespan DESC
-
---  IFNULL(`split`, 2020) - `formed`
--- Write a SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
-
--- Requirements:
-
-
--- Column names must be: band_name and lifespan (in years)
--- You should use attributes formed and split for computing the lifespan
--- Your script can be executed on any database

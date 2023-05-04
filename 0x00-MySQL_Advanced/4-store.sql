9d7386ef0566d3d082380d4dec4d219a2c94cfb2
Madele <madele.theron@gmail.com>
2023-05-04 21:11:33 +0200

    remove begin & end

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -2,8 +2,6 @@
 -- quantity of an item after adding a new order.:
 CREATE TRIGGER decrease_quantity BEFORE INSERT ON orders
 FOR EACH ROW
-BEGIN
     UPDATE items
     SET quantity = quantity - NEW.quantity_ordered
     WHERE item_name = NEW.item_name;
-END;

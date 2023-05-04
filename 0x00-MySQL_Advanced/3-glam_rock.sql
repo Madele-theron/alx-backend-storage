1d5574c59f7d0e438dc92758db4e49aef7b4e37f
Madele <madele.theron@gmail.com>
2023-05-04 19:57:20 +0200

    comment out lifesona order

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,4 +2,4 @@
 -- Create a table called "users"
 SELECT DISTINCT band_name, DATEDIFF(IFNULL(split, NOW()), formed) as lifespan 
 FROM metal_bands WHERE FIND_IN_SET('Glam rock', style)
-ORDER BY lifespan DESC
+-- ORDER BY lifespan DESC

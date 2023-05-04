a0d09dd6294659f4872140895553fa22c15e22be
Madele <madele.theron@gmail.com>
2023-05-04 20:04:08 +0200

    .

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
--- A SQL Script that ranks country origins of bands by fans:
--- Create a table called "users"
-SELECT DISTINCT band_name, IFNULL(split, 2023) - IFNULL(formed, 0) as lifespan 
+-- A SQL Script that shows all bands that has Glam Rock as style:
+-- in order of lifespan
+SELECT band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan 
 FROM metal_bands 
-WHERE FIND_IN_SET('Glam rock', style)
+WHERE style LIKE '%Glam rock%';

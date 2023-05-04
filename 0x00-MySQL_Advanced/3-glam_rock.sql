aea33b73a04134b62ac20b9a689347cc71debae4
Madele <madele.theron@gmail.com>
2023-05-04 20:00:40 +0200

    position of string greater than 0

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- A SQL Script that ranks country origins of bands by fans:
 -- Create a table called "users"
 SELECT DISTINCT band_name, DATEDIFF(IFNULL(split, NOW()), formed) as lifespan 
-FROM metal_bands WHERE FIND_IN_SET('Glam rock', style)
--- ORDER BY lifespan DESC
+FROM metal_bands 
+WHERE FIND_IN_SET('Glam rock', style) > 0

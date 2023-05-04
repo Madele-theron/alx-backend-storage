18791b5e7fe2fd1dadd40aeb82f0bfc876fbee2f
Madele <madele.theron@gmail.com>
2023-05-04 20:02:45 +0200

    change calculation for lifespan

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- A SQL Script that ranks country origins of bands by fans:
 -- Create a table called "users"
-SELECT DISTINCT band_name, DATEDIFF(IFNULL(split, NOW()), formed) as lifespan 
+SELECT DISTINCT band_name, IFNULL(split, 2023) - IFNULL(formed, 0) as lifespan 
 FROM metal_bands 
-WHERE FIND_IN_SET('Glam rock', style) > 0
+WHERE FIND_IN_SET('Glam rock', style)

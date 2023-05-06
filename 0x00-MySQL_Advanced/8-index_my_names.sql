68a0436db37c290d7f1ab9abea108fdcf5f85f70
Madele <madele.theron@gmail.com>
2023-05-06 20:10:27 +0200

    task 8

---
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -0,0 +1,4 @@
+-- SQL script that creates an index idx_name_first on the table names
+-- and the first letter of name.
+-- create the index if it does not exist
+CREATE INDEX IF NOT EXISTS idx_name_first ON names (LEFT(name, 1));

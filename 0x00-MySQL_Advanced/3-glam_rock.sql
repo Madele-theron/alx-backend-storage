96d150554a80665d8b5fe2086b88953270da5ef9
Madele <madele.theron@gmail.com>
2023-05-06 20:17:14 +0200

    capitalize AS

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- A SQL Script that shows all bands that has Glam Rock as style:
 -- in order of lifespan
-SELECT DISTINCT band_name, DATEDIFF(IFNULL(split, NOW()), formed) as lifespan 
+SELECT band_name, DATEDIFF(IFNULL(split, NOW()), formed) AS lifespan 
 FROM metal_bands 
 WHERE style LIKE '%Glam rock%';

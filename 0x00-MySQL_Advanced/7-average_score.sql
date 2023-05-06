d90a287d9178c3abd60416e06bbd419ac415e318
Madele <madele.theron@gmail.com>
2023-05-06 19:57:31 +0200

    change spacing between '='

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -2,14 +2,13 @@
 -- that computes and store the average score for a student
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$
-
 CREATE PROCEDURE ComputeAverageScoreForUser ( 
     IN user_id INT
 )
 BEGIN
     DECLARE avrg_score FLOAT;
-    SET avrg_score = (SELECT AVG(score) FROM corrections WHERE user_id = user_id);
-    UPDATE users SET average_score = avrg_score WHERE id = user_id;
+    SET avrg_score = (SELECT AVG(score) FROM corrections WHERE user_id=user_id);
+    UPDATE users SET average_score = avrg_score WHERE id=user_id;
 END $$
-
 DELIMITER ;
+

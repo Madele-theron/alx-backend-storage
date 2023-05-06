8f252ddcb3912c8f63229da6ba5123a8be5c30e9
Madele <madele.theron@gmail.com>
2023-05-06 20:20:55 +0200

    change year

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- A SQL Script that shows all bands that has Glam Rock as style:
 -- in order of lifespan
-SELECT band_name, IFNULL(split, 2023), IFNULL(formed, 0) AS lifespan 
+SELECT band_name, IFNULL(split, 2020), IFNULL(formed, 0) AS lifespan 
 FROM metal_bands 
 WHERE style LIKE '%Glam rock%';

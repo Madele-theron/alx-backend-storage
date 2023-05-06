f4640606cc072234c225e26afd531cb7c092a18f
Madele <madele.theron@gmail.com>
2023-05-06 21:21:16 +0200

    update return type

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,8 +1,9 @@
 -- A SQL Script that creates a function SafeDiv that divides (and returns)
 -- the first by the second number or returns 0 if the second number is equal to 0.
+DROP FUNCTION IF EXISTS SafeDiv;
 DELIMITER $$
 CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS INT
+RETURNS FLOAT DETERMINISTIC
 BEGIN
     IF b <> 0 THEN
         RETURN a/b

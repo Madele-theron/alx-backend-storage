362d1e447cb432de2a1615d33ebfa6a2cf33859a
Madele <madele.theron@gmail.com>
2023-05-06 20:12:24 +0200

    remove if exist statement

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,4 +1,3 @@
 -- SQL script that creates an index idx_name_first on the table names
 -- and the first letter of name.
--- create the index if it does not exist
-CREATE INDEX IF NOT EXISTS idx_name_first ON names (name(1));
+CREATE INDEX idx_name_first ON names (name(1));

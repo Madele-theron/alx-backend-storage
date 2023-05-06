911919596760d275a6eb97dcea58d2ed1748d035
Madele <madele.theron@gmail.com>
2023-05-06 20:42:11 +0200

    remove decrease_quantity

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,6 +1,6 @@
 -- A SQL Script that creates a trigger that decreases the 
 -- quantity of an item after adding a new order.:
-CREATE TRIGGER DelQuantityConsult decrease_quantity AFTER INSERT ON orders
+CREATE TRIGGER DelQuantityConsult AFTER INSERT ON orders
 FOR EACH ROW
     UPDATE items
     SET quantity = quantity - NEW.quantity_ordered

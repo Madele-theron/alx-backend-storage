50a6c794136afa9e134eb46d7d0557fb311a20eb
Madele <madele.theron@gmail.com>
2023-05-09 23:29:45 +0200

    task 102

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -34,41 +34,23 @@ if __name__ == "__main__":
             "$group":
                 {
                     "_id": "$ip",
-                    
+                    "count": {"$sum": 1}
                 }
+        },
+        {
+            "$sort": {"count": -1}
+        },
+        {
+            "$limit": 10
+        },
+        {
+            "$project": {
+                "_id": 0,
+                "ip": "$_id",
+                "count": 1
+            }
         }
     ])
     print("IPs:")
-    
-
-    print("IPs:")
-    top_ips = collection.aggregate([
-        {"$group":
-            {
-                "_id": "$ip",
-                "count": {"$sum": 1}
-            }
-        },
-        {"$sort": {"count": -1}},
-        {"$limit": 10},
-        {"$project": {
-            "_id": 0,
-            "ip": "$_id",
-            "count": 1
-        }}
-    ])
-    for ip in top_ips:
+    for ip in ip_list:
         print(f"\t{ip.get('ip')}: {ip.get('count')}")
-
-
-# IPs:
-#     172.31.63.67: 15805
-#     172.31.2.14: 15805
-#     172.31.29.194: 15805
-#     69.162.124.230: 529
-#     64.124.26.109: 408
-#     64.62.224.29: 217
-#     34.207.121.61: 183
-#     47.88.100.4: 166
-#     45.249.84.250: 160
-#     216.244.66.228: 150

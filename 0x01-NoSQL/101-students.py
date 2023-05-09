2a8d78854aa94d8418e48dde99d18d426cf049b4
Madele <madele.theron@gmail.com>
2023-05-09 23:13:14 +0200

    task 101

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""A Python function that returns all students sorted by average score:
+"""
+
+
+def top_students(mongo_collection):
+    """List all students sorted by their average score
+
+    Args:
+        mongo_collection (pymongo collection object): MongoDB collection
+
+    Returns:
+        All students sorted by average score
+    """
+
+    student_list = mongo_collection.aggregated([
+        {
+            "$projects": {
+                "name": "$name",
+                "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {
+            "$sort": {"averageScore": -1}
+        }
+    ])
+    
+    return student_list

db76c9c9f801baafac560e030f0e581eb1a9304e
Madele <madele.theron@gmail.com>
2023-05-10 19:25:28 +0200

    fix pycodestyle errors

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,13 +4,13 @@ import redis
 from typing import Union
 from uuid import uuid4
 
+
 class Cache:
     """ Writing strings to Redis """
     def __init__(self):
         """Instance of the Redis client as a private variable """
         self._redis = redis.Redis()
         self._redis.flushdb
-
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Generate random key and store input data in Redis"""

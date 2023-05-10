59c2964bee16d33205064c0b0897b9caf2314c84
Madele <madele.theron@gmail.com>
2023-05-10 19:44:02 +0200

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Redis task solutions"""
 import redis
-from typing import Union
+from typing import Union, Callable, Optional
 from uuid import uuid4
 
 
@@ -17,3 +17,22 @@ class Cache:
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """Convert the data back to the desired format"""
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """Auto parametrize Cache.get with correct conversion function."""
+        data = self._redis.get(key)
+        return data.decode('utf-8')
+
+    def get_int(self, key: str) -> int:
+        """Auto parametrize Cache.get with correct conversion function."""
+        data = self._redis.get(key)
+        return data.get(key, int)

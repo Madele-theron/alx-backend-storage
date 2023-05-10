4edd75ceb818700cb3d536ce2f82e2abc630e5b9
Madele <madele.theron@gmail.com>
2023-05-10 19:18:02 +0200

    task 0

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" Redis task solutions
+"""
+from typing import Union
+import redis
+import uuid4
+
+class Cache:
+    """
+    Writing strings to Redis
+    """
+    def __init__(self):
+        """Instance of the Redis client as a private variable """
+        self._redis = redis.Redis()
+        self._redis.flushdb
+
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Generate random key and store input data in Redis"""
+        key = str(uuid4())
+        self._redis.set(key, data)
+        return key

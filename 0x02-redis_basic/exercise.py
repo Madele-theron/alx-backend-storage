6edf5ec041a56d915b0388950ca211c697cb86ab
Madele <madele.theron@gmail.com>
2023-05-10 19:23:57 +0200

    improve spacing

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
-""" Redis task solutions
-"""
-from typing import Union
+""" Redis task solutions"""
 import redis
+from typing import Union
 from uuid import uuid4
 
 class Cache:
-    """
-    Writing strings to Redis
-    """
+    """ Writing strings to Redis """
     def __init__(self):
         """Instance of the Redis client as a private variable """
         self._redis = redis.Redis()
